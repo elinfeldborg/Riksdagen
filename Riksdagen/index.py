4806e33af189547f627ac9a2903ccbab1cf8fecf
ag7511 <elinfeldborg@msn.com>
2017-10-03 14:15:12 +0200

    Python

--- Riksdagen/index.py
+++ Riksdagen/index.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import flask, render_template
 
 app = Flask(__name__)
@@ -13,3 +14,19 @@ def member.html():
 
 if __name__ == "__main__":
     app.run(debug=True)
+=======
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path = "",static_folder= "")
+
+@app.route('/')
+def index_html():
+   return render_template('index.html')
+
+@app.route('/members')
+def memebers_html():
+   return render_template('memebers.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
+>>>>>>> Stashed changes
