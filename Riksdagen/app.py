e05485e73256003cf223f8cb5f0e9101e52b5039
matildasundblom <matilda.sundblom@hotmail.com>
2017-10-18 14:23:39 +0200

    ändringar

--- Riksdagen/app.py
+++ Riksdagen/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, json, render_template
+import requests
 
 app = Flask(__name__, static_url_path = "",static_folder= "")
 
@@ -6,9 +7,10 @@ app = Flask(__name__, static_url_path = "",static_folder= "")
 def index_html():
    return render_template('index.html')
 
-@app.route('/members')
+@app.route('/members', methods=['GET', 'POST'])
 def members_html():
-   return render_template('members.html')
+    response = requests.get("http://data.riksdagen.se/personlista/?iid=&fnamn=Matilda&enamn=Sundblom&f_ar=1992&kn=kvinna&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista=parti5")
+    return render_template('members.html')
 
 @app.route('/contact')
 def contact_html():
