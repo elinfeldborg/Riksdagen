0ac8e558dca331a34c723397d6ac738476109c47
ag7511 <elinfeldborg@msn.com>
2017-10-18 14:29:10 +0200

    Tog bort konflikt

--- Riksdagen/app.py
+++ Riksdagen/app.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-from flask import Flask, json, render_template
-import requests
-=======
 from flask import Flask, render_template, request, json
->>>>>>> 725e1c166cdda6726db15989954c7b3ea126df65
 
 app = Flask(__name__)
 
@@ -13,15 +8,11 @@ def index_html():
 
 @app.route('/members', methods=['GET', 'POST'])
 def members_html():
-<<<<<<< HEAD
-    response = requests.get("http://data.riksdagen.se/personlista/?iid=&fnamn=Matilda&enamn=Sundblom&f_ar=1992&kn=kvinna&parti=&valkrets=&rdlstatus=&org=&utformat=json&termlista=parti5")
-=======
 
     response = request.args.get('http://data.riksdagen.se/personlista/?iid=&fnamn=Elin&enamn=Feldborg+Nielsen&f_ar=1994&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=html&termlista=parti')
     ledarmoter = response.json()
     print(ledarmoter)
 
->>>>>>> 725e1c166cdda6726db15989954c7b3ea126df65
     return render_template('members.html')
 
 @app.route('/contact')
