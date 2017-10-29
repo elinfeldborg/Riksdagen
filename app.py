472128069f59a72ccd7dbd22ad2b39dcbc3704de
ag7511 <elinfeldborg@msn.com>
2017-10-29 14:59:25 +0100

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 from jinja2 import Template
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
-=======
-from flask import Flask, render_template, json
->>>>>>> origin/master
-import requests
 
 app = Flask(__name__)
 
@@ -15,26 +10,17 @@ app = Flask(__name__)
 def index_html():
     return render_template('index.html')
 
-@app.route('/members', methods = ['GET'])
+@app.route('/members')
 def members_html():
 
     response = requests.get("http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=KD&valkrets=&rdlstatus=&org=&utformat=json&termlista=")
     members = response.json()
-<<<<<<< HEAD
-      
+
     return render_template("members.html", members = members)
-=======
-
-
-
-    return render_template('members.html', members=members)
->>>>>>> origin/master
 
 @app.route('/contact')
 def contact_html():
     return render_template('contact.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug = True)
